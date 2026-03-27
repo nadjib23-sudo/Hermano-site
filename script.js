812b99e3590470167ccb4bd51f7ec0eb8a1d20e5
nadjib23-sudo <nadjibdaoudi2005@gmail.com>
2026-03-27 23:38:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ window.onload = function() {
                 total: cart.reduce((sum, item) => sum + item.price, 0) + " د.ج"
             };
 
-            const scriptURL = 'https://script.google.com/macros/s/AKfycbxvdJn4nIRjs9Cql3_jk-ESU27SOE6kdim-bMnDNfGWyDTdgafcpRnBmqgStzJje-NP/exec';
+            const scriptURL = 'https://script.google.com/macros/s/AKfycbwmzBv5uxQqcLGyS4XVSFn8MQTRfSaO5eBturPa2cFhZOmkcsXjhDIPf6mPU1o19kh7/exec';
 
             fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: JSON.stringify(data) })
             .then(() => {
